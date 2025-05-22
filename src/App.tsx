4680399b5aca04764df04c1e4c781c6bc4d8aaee
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-05-22 08:35:09 -0300

    use state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {Button, SafeAreaView, StyleSheet} from 'react-native';
 import Header from './componets/Header/Index'; // Corrigi "componets" para "components"
 
 const App = () => {
     const [name, setName] = useState<string>('Nicolas');
+    const [title, setTitle] = useState<string>('LearningReact');
+
+    //Isso é uma função callback, pois estou passando como parametro
+    /* Callback */
+
+    //Minha forma
+    /* function onButtonClick() {
+        if (name === 'Nicolas') setName('Oliver');
+        else setName('Nicolas');
+    } */
+    const handlePressButton = () => {
+        //assim voce gerencia o estado do obj certinho garantindo que seja uma
+        //das duas
+        setName(old => {
+            if (old === 'Nicolas') return 'oliver';
+            else return 'Nicolas';
+        });
+    };
     return (
         <SafeAreaView style={styles.App}>
-            <Header name={name} title="learning React Native" />
-            <Header name="VNL" title="LIGA DAS NAÇÕES" />
+            <Header name="VNL" title="LIGA DS NAÇÕES" />
+            <Button
+                title="Change Name"
+                /* onPress={onButtonClick} */ onPress={
+                    handlePressButton
+                }></Button>
         </SafeAreaView>
     );
 };
